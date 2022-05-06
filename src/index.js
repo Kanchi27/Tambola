a11957d31ec5bbb20b500a9d95162789f900d784
Joby Joseph <reachjoby@gmail.com>
2022-05-07 02:54:04 +0530

    Styled Components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import TambolaCell from "./components/tambola-cell";
 
 const App = () => {
-  return <h1>Tambola Game!</h1>;
+  return (
+    <div>
+      <h1>Tambola Game!</h1>
+      <TambolaCell />
+      <br />
+      <TambolaCell numberCalled />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
